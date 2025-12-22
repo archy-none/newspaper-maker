89e1af08115ea6df935511a58c144e71cf21caaf
秋井乃音斗 <archy1none@gmail.com>
2025-12-22 18:03:21 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,49 @@
+var activeItem = null;
+
+function isolate() {
+  for (item of root.children) {
+    item.className = "";
+  }
+}
+
+function createItem() {
+  let el = document.createElement("div");
+  let root = document.getElementById("root");
+  root.appendChild(el);
+
+  el.onclick = () => {
+    activeItem = el;
+    if (el.classList.contains("write")) {
+      write(activeItem);
+    } else {
+      read(activeItem);
+    }
+
+    isolate();
+    el.classList.toggle("write");
+  };
+  new DragMoveResize(el);
+  write(el);
+}
+
+function write(el) {
+  let size = document.getElementById("size");
+  el.style.fontSize = `${size.value}px`;
+  let color = document.getElementById("color");
+  el.style.color = color.value;
+  let back = document.getElementById("back");
+  el.style.backgroundColor = back.value;
+  let value = document.getElementById("value");
+  el.innerText = value.value;
+}
+
+function read(el) {
+  let size = document.getElementById("size");
+  size.value = parseInt(el.style.fontSize.replace("px"));
+  let color = document.getElementById("color");
+  color.value = el.style.color;
+  let back = document.getElementById("back");
+  back.value = el.style.backgroundColor;
+  let value = document.getElementById("value");
+  value.value = el.innerText;
+}
